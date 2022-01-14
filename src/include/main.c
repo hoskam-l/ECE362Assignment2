f360f7bea912143017772587dc27625a441d48a0
luke <hoskam@pdx.edu>
2022-01-14 09:17:10 -0800

    added the ability to open a file.

--- src/include/main.c
+++ src/include/main.c
@@ -1,14 +1,37 @@
+#ifdef _WIN32
 #include <io.h>
+#else
+#include <unistd.h>
+#endif
+
+
+#include <errno.h>
 #include <stdio.h>
 #include <string.h>
 #include <fcntl.h>
 
+#define BUFFSIZE  4096
 
-int main()
+void err_sys(char* msg)
 {
-    int fd;
-    int sz;
-    char *c = (char *) callooc(100, sizeof(char));
+	fprintf(stderr, "%s (%s)\n", msg, strerror(errno));
+	exit(1);
+}
 
-    fd = open("output.log", O_RDONLY);
+
+int main(int argc, char *argv[])
+{
+    int n;
+	char buf[BUFFSIZE];
+    int filedesc = open("testfile.txt", O_WRONLY | O_APPEND);
+ 
+    if (filedesc < 0) {
+        return -1;
+    }
+
+    while ((n = read(STDIN_FILENO, buf, BUFFSIZE)) > 0)
+		if (write(STDOUT_FILENO, buf, n) != n)
+			err_sys("write error");
+
+	if (n < 0) err_sys("read error");
 }
