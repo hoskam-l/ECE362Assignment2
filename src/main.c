e26c8c621801a5e4315182045bfe9327461b9375
Luke Hoskam <hoskam@auto.ece.pdx.edu>
2022-01-14 09:39:18 -0800

    added file

---
+++ src/main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#ifdef _WIN32
+#include <io.h>
+#else
+#include <unistd.h>
+#endif
+#include <string.h>
+#include <errno.h>
+
+#define BUFFSIZE  4096
+
+void err_sys(char* msg)
+{
+	fprintf(stderr, "%s (%s)\n", msg, strerror(errno));
+	exit(1);
+}
+
+int main(void)
+{
+	int n;
+	char buf[BUFFSIZE];
+
+	while ((n = read(STDIN_FILENO, buf, BUFFSIZE)) > 0)
+		if (write(STDOUT_FILENO, buf, n) != n)
+			err_sys("write error");
+
+	if (n < 0) err_sys("read error");
+
+	exit(0);
+}
