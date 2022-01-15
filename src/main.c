aa3a7bec46ee729211c8bafe142d51c097620507
luke <hoskam@pdx.edu>
2022-01-15 05:21:58 -0800

    added write to file

--- src/main.c
+++ src/main.c
@@ -7,6 +7,8 @@
 #endif
 #include <string.h>
 #include <errno.h>
+#include <fcntl.h>
+
 
 #define BUFFSIZE  4096
 
@@ -20,9 +22,13 @@ int main(void)
 {
 	int n;
 	char buf[BUFFSIZE];
-
+	 int filedesc = open("testfile.txt", O_WRONLY | O_APPEND);
+ 
+    if (filedesc < 0) {
+        return -1;
+    }
 	while ((n = read(STDIN_FILENO, buf, BUFFSIZE)) > 0)
-		if (write(STDOUT_FILENO, buf, n) != n)
+		if ((write(STDOUT_FILENO, buf, n) || (write(filedesc, buf, n))!= n))
 			err_sys("write error");
 
 	if (n < 0) err_sys("read error");
