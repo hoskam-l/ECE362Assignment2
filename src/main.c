f412c0164e14678013c9ed421d953916d8d8ad11
luke <hoskam@pdx.edu>
2022-01-15 06:05:55 -0800

    na

--- src/main.c
+++ src/main.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #ifdef _WIN32
-#include <io.h>
+	#include <io.h>
+	#define STDIN_FILENO 0
+	#define STDOUT_FILENO 1
+	#define STDERR_FILENO 2
 #else
-#include <unistd.h>
+	#include <unistd.h>
 #endif
 #include <string.h>
 #include <errno.h>
-#include <fcntl.h>
-
 
 #define BUFFSIZE  4096
 
@@ -22,13 +23,9 @@ int main(void)
 {
 	int n;
 	char buf[BUFFSIZE];
-	 int filedesc = open("testfile.txt", O_WRONLY | O_APPEND);
- 
-    if (filedesc < 0) {
-        return -1;
-    }
+
 	while ((n = read(STDIN_FILENO, buf, BUFFSIZE)) > 0)
-		if ((write(STDOUT_FILENO, buf, n) || (write(filedesc, buf, n))!= n))
+		if (write(STDOUT_FILENO, buf, n) != n)
 			err_sys("write error");
 
 	if (n < 0) err_sys("read error");
